aee30a9c5b0da904165564b97b6cc63c7450444b
Harshit Gulati <harshitgulati6126@gmail.com>
2025-06-05 17:40:16 +0530

    removed map.js

--- map.js
+++
@@ -1,8 +0,0 @@
-const peoples = [
-  { name: "Alice", age: 25 },
-  { name: "Bob", age: 30 },
-];
-
-peoples.map((peoples) =>
-  console.log(`name: ${peoples.name} age: ${peoples.age}`)
-);
--- src/Body.js
+++ src/Body.js
@@ -1,16 +1,40 @@
+import { useState } from "react";
 import RestaurantCard from "./RestrauntCard.js";
 import { RestaurantData } from "./config.js";
 
 const Body = () => {
-  console.log("lalalala");
+  const [searchData, setSearchData] = useState(RestaurantData);
+  const searchHandel = (searchTxt, searchData) => {
+    const filteredData = searchData.filter((i) =>
+      i.info.name.includes(searchTxt)
+    );
+    setSearchData(filteredData);
+  };
+  const [searchTxt, setsearchTxt] = useState("");
   return (
-    <div className="body">
-      {RestaurantData.slice(0, 16).map((data) => {
-        return (
-          <RestaurantCard props={data.info} key={data.info.id} role="link" />
-        );
-      })}
-    </div>
+    <>
+      <input
+        type="search"
+        placeholder="Search"
+        value={searchTxt}
+        onChange={(e) => setsearchTxt(e.target.value)}
+      />
+      <button
+        onClick={() => {
+          searchHandel(searchTxt, searchData);
+          console.log(searchData);
+        }}
+      >
+        button
+      </button>
+      <div className="body">
+        {searchData.slice(0, 16).map((data) => {
+          return (
+            <RestaurantCard props={data.info} key={data.info.id} role="link" />
+          );
+        })}
+      </div>
+    </>
   );
 };
 export default Body;
